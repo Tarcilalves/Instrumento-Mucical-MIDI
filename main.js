955f080d5aa32419730412975f8b116975683de8
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2023-04-21 13:13:09 -0300

    iniciando uma lista

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 const listaDeTeclas=document.querySelectorAll('.tecla');
+
+//lista
+
+listaDeTeclas[0].onclick=tocaSomPom;
+
+
